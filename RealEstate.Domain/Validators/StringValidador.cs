c74fe9f8e54a4806fe0a3d032316e45492b34869
sergiowgt2 <sergiows.rs@gmail.com>
2024-04-18 12:09:43 -0300

    String Validator

---
+++ RealEstate.Domain/Validators/StringValidador.cs
@@ -0,0 +1,24 @@
+﻿using RealEstate.Domain.Exceptions;
+
+namespace RealEstate.Domain.Validators;
+
+public class StringValidador
+{
+    public static void Validate(string toValidate, string fieldName, int exactSize = 0, int minSize = 0, int maxSize = 0)
+    {
+        toValidate = toValidate.Trim();
+        if (exactSize > 0)
+        {
+            if (toValidate.Length != exactSize)
+                throw new DomainException($"{fieldName} must be exact size of - {exactSize}");
+        }
+        else
+        {
+            if (maxSize > 0 && toValidate.Length < minSize)
+                throw new DomainException($"{fieldName} minimum size is - {minSize}");
+
+            if (maxSize > 0 && toValidate.Length > maxSize)
+                throw new DomainException($"{fieldName} maximum size is - {maxSize}");
+        }
+    }
+}
